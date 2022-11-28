4a575832d612cef15301cd07ce5057978e3d6e7f
Mohammad Opu <mr95opu@gmail.com>
2022-11-28 15:48:43 +0600

    APIs connection done with localhost

--- app.js
+++ app.js
@@ -40,6 +40,13 @@ app.post("/",function(req,res){
     }
 
     const request = https.request(url,options,function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        } else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
         response.on("data",function(data){
             console.log(JSON.parse(data));
         });
@@ -49,11 +56,17 @@ app.post("/",function(req,res){
     request.end();
 });
 
+app.post("/failure" , function(req,res){
+    res.redirect("/");
+});
+
 
 app.listen(3000,function(){
     console.log("The server is running on 3000.");
 });
 
+// Api key
 // 9880a712ef829e977e7219078f242b72-us14
 
+// user id
 // 529a0bab86
